9dba4429d5cb2d7b0792ca3ee9c19cf578abe156
Thijs Brobbel <thijsbrobbel@gmail.com>
2011-10-24 16:07:56 +0200

    Added support for overring the project_environment.bat
    Updated BuildSmallworldImage.rb accordingly.

--- start_gis.bat
+++ start_gis.bat
@@ -1,3 +1,5 @@
-@call %~dp0build_environment.bat
-@call %~dp0project_environment.bat
-@%gis% -e environment.bat %*
+@echo off
+call %~dp0build_environment.bat
+call %PROJECT_DIR%\project_environment.bat
+if exist %PROJECT_DIR%\my_project_environment.bat call %PROJECT_DIR%\my_project_environment.bat
+%gis% -e environment.bat %*
