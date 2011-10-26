f043313acc908e75890485f867d34730b247ad18
Thijs Brobbel <thijsbrobbel@gmail.com>
2011-10-26 19:43:26 +0200

    Fixes not being able to override env vars
    When using my_project_environment.bat, you can override the
    configuration of the project (project_environment.bat), however, using
    the "@if not defined " construct implemented @SAB-7660, this behavior
    was effectively gone. This fixes that.
    GEM:7660

--- start_gis.bat
+++ start_gis.bat
@@ -1,5 +1,5 @@
 @echo off
 call %~dp0bin\share\build_environment.bat
-call %PROJECT_DIR%\project_environment.bat
 if exist %PROJECT_DIR%\my_project_environment.bat call %PROJECT_DIR%\my_project_environment.bat
+call %PROJECT_DIR%\project_environment.bat
 %gis% -e environment.bat %*
