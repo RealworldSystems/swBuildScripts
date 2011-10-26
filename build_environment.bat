f6814f018fde3a3019bbc3c76efbba9d69ddc952
Thijs Brobbel <thijsbrobbel@gmail.com>
2011-10-26 16:15:06 +0200

    Changes usage of $PROJECT_DIR
    The convention for $PROJECT_DIR has changed, it always *includes* the
    trailing slash. Making changes accordingly.
    GEM:7659

--- build_environment.bat
+++ build_environment.bat
@@ -1,7 +1,7 @@
 set PROJECT_DIR=%~dp0
 
-set SW_GIS_ALIAS_FILES=%PROJECT_DIR%\config\magik_images\resources\base\data\gis_aliases
-set LOG_DIR=%PROJECT_DIR%\log
-set SW_SAVE_IMAGE_DIR=%PROJECT_DIR%\images
+set SW_GIS_ALIAS_FILES=%PROJECT_DIR%config\magik_images\resources\base\data\gis_aliases
+set LOG_DIR=%PROJECT_DIR%log
+set SW_SAVE_IMAGE_DIR=%PROJECT_DIR%images
 set SW_SAVE_IMAGE_DIR_FORMAT=spin
 set SPIN=main
