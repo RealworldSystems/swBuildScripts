6a0059f791d44597b0df1f51652b42af24ba793c
Thijs Brobbel <thijsbrobbel@gmail.com>
2011-10-26 17:51:43 +0200

    Relocates BUILD_IMAGE.cmd to bin/share.
    This keeps the root dir clean.
    GEM:7734

--- bin/share/redirect_output.rb
+++ bin/share/redirect_output.rb
@@ -1,3 +1,3 @@
 # FIXME: inline this somehow into BuildSmallworldImage.rb
-ARGV[1] = 'BUILD_IMAGE.cmd'
+ARGV[1] = 'bin\share\BUILD_IMAGE.cmd'
 system({"COMSPEC" => ENV["COMSPEC_OLD"]}, "cmd.exe " + ARGV.join(" "))
